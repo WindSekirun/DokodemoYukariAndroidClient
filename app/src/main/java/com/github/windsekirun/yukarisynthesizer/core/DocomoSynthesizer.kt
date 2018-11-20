4751e179f9facc4c31469f89f0becc47f7af70b1
Pyxis <pyxis@uzuki.live>
2018-11-20 10:33:34 +0900

    implement writeWavHeader in DocomoSynthesizer

--- app/src/main/java/com/github/windsekirun/yukarisynthesizer/core/DocomoSynthesizer.kt
+++ app/src/main/java/com/github/windsekirun/yukarisynthesizer/core/DocomoSynthesizer.kt
@@ -1,8 +1,17 @@
 package com.github.windsekirun.yukarisynthesizer.core
 
+import android.content.Context
+import com.github.windsekirun.yukarisynthesizer.core.item.SSMLItem
 import okhttp3.*
+import okio.Okio
+import pyxis.uzuki.live.richutilskt.utils.asDateString
 import pyxis.uzuki.live.richutilskt.utils.runAsync
+import java.io.File
 import java.io.IOException
+import java.io.OutputStream
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
 
 /**
  * DokodemoYukariAndroidClient
@@ -13,10 +22,15 @@ import java.io.IOException
  */
 object DocomoSynthesizer {
 
-    fun process(ssml: String, apiKey: String) {
+    fun process(context: Context, item: SSMLItem, apiKey: String) {
         runAsync {
-            val url = "https://api.apigw.smt.docomo.ne.jp/aiTalk/v1/textToSpeech?APIKEY=$apiKey"
+            val pcmFile = File(
+                context.externalCacheDir,
+                "/pcm/%s.pcm".format(System.currentTimeMillis().asDateString())
+            )
 
+            val url = "https://api.apigw.smt.docomo.ne.jp/aiTalk/v1/textToSpeech?APIKEY=$apiKey"
+            val ssml = SSMLBuilder.process(item, true)
             val body = RequestBody.create(MediaType.parse("application/ssml+xml"), ssml)
 
             val request = Request.Builder().url(url)
@@ -33,10 +47,49 @@ object DocomoSynthesizer {
                 }
 
                 override fun onResponse(call: Call, response: Response) {
+                    val sink = Okio.buffer(Okio.sink(pcmFile))
+                    sink.writeAll(response.body()?.source())
+                    sink.close()
 
+                    encodeWav(context, item)
                 }
-
             })
         }
+    }
+
+    private fun encodeWav(context: Context, item: SSMLItem) {
+
+    }
+
+    private fun writeWavHeader(
+        outputStream: OutputStream, channels: Short,
+        sampleRate: Int, bitDepth: Short
+    ) {
+        val littleBytes = ByteBuffer
+            .allocate(14)
+            .order(ByteOrder.LITTLE_ENDIAN)
+            .putShort(channels)
+            .putInt(sampleRate)
+            .putInt(sampleRate * channels * (bitDepth / 8))
+            .putShort((channels * (bitDepth / 8)).toShort())
+            .putShort(bitDepth)
+            .array()
+
+
+        outputStream.write(byteArrayOf(
+            'R'.toByte(), 'I'.toByte(), 'F'.toByte(), 'F'.toByte(), // Chunk ID
+            0, 0, 0, 0, // Chunk Size
+            'W'.toByte(), 'A'.toByte(), 'V'.toByte(), 'E'.toByte(), // Format
+            'f'.toByte(), 'm'.toByte(), 't'.toByte(), ' '.toByte(), //Chunk ID
+            16, 0, 0, 0, // Chunk Size
+            1, 0, // AudioFormat
+            littleBytes[0], littleBytes[1], // Num of Channels
+            littleBytes[2], littleBytes[3], littleBytes[4], littleBytes[5], // SampleRate
+            littleBytes[6], littleBytes[7], littleBytes[8], littleBytes[9], // Byte Rate
+            littleBytes[10], littleBytes[11], // Block Align
+            littleBytes[12], littleBytes[13], // Bits Per Sample
+            'd'.toByte(), 'a'.toByte(), 't'.toByte(), 'a'.toByte(), // Chunk ID
+            0, 0, 0, 0 //Chunk Size
+        ))
     }
 }
--- app/src/main/java/com/github/windsekirun/yukarisynthesizer/core/item/SSMLItem.kt
+++ app/src/main/java/com/github/windsekirun/yukarisynthesizer/core/item/SSMLItem.kt
@@ -2,12 +2,16 @@ package com.github.windsekirun.yukarisynthesizer.core.item
 
 import io.objectbox.annotation.Entity
 import io.objectbox.annotation.Id
+import java.util.*
 
 @Entity
 class SSMLItem {
     @Id
     var id: Long = 0
     var version: String = "1.1"
+    var title: String = ""
+    var regDate: Date = Date()
+    var localPath: String = ""
     var voices: List<VoiceItem> = mutableListOf()
 
     constructor(voices: List<VoiceItem>) {
