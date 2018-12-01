e4693dd93b43f6c4065c615f1655e9cba1827bff
Pyxis <pyxis@uzuki.live>
2018-12-01 15:04:34 +0900

    remove ai

--- app/src/main/java/com/github/windsekirun/yukarisynthesizer/core/define/VoiceEngine.kt
+++ app/src/main/java/com/github/windsekirun/yukarisynthesizer/core/define/VoiceEngine.kt
@@ -5,7 +5,6 @@ import com.github.windsekirun.yukarisynthesizer.core.base.PropertyEnumConverter
 enum class VoiceEngine(val id: String, val originName: String) {
     Yukari("Yukari", "sumire"),
     Maki("Maki", "maki"),
-    Ai("Ai", "anzu"),
     NONE("NONE", "none");
 
     class VoiceEngineConverter : PropertyEnumConverter<VoiceEngine, String>(
--- app/src/main/java/com/github/windsekirun/yukarisynthesizer/core/test/TestData.kt
+++ app/src/main/java/com/github/windsekirun/yukarisynthesizer/core/test/TestData.kt
@@ -18,7 +18,6 @@ fun test1(): StoryItem {
     // normal preset
     val yukari = PresetItem(VoiceEngine.Yukari, 1.4, 1.2)
     val maki = PresetItem(VoiceEngine.Maki, 1.4)
-    val ai = PresetItem(VoiceEngine.Ai, 1.4)
 
     // content from https://www.nicovideo.jp/watch/sm30193805
     val yukari1 = buildVoiceItem(VoiceEngine.Yukari, yukari) {
@@ -30,16 +29,8 @@ fun test1(): StoryItem {
         voice("マキです")
     }
 
-    val ai1 = buildVoiceItem(VoiceEngine.Ai, ai) {
-        voice("ゆっくり霊夢です")
-    }
-
     val yukari2 = buildVoiceItem(VoiceEngine.Yukari, yukari) {
         voice("突然ですけど私、スーパーハカーになりました！")
-    }
-
-    val ai2 = buildVoiceItem(VoiceEngine.Ai, ai) {
-        voice("この人いきなり何言ってんだ…")
     }
 
     val maki2 = buildVoiceItem(VoiceEngine.Maki, maki) {
@@ -63,21 +54,19 @@ fun test1(): StoryItem {
         voice("ドライブの中身を晒してやる！")
     }
 
-    val ai3 = buildVoiceItem(VoiceEngine.Ai, ai) {
+    val maki4 = buildVoiceItem(VoiceEngine.Maki, maki) {
         voice("やめてください！社会的に死ぬ人が出るのでやめてください！")
     }
 
     val voices = mutableListOf<VoiceItem>().apply {
         this.add(yukari1)
         this.add(maki1)
-        this.add(ai1)
         this.add(yukari2)
-        this.add(ai2)
         this.add(maki2)
         this.add(maki3)
         this.add(yukari3)
         this.add(yukari4)
-        this.add(ai3)
+        this.add(maki4)
     }
 
     return StoryItem("sm30193805", voices);
