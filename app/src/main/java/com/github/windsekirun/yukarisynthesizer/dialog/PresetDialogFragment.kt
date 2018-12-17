396b40ba42c1d65140cf8fd9f585daaf61fb5cbc
Pyxis <pyxis@uzuki.live>
2018-12-17 21:11:00 +0900

    fix some point in PresetDialogFragment

--- app/src/main/java/com/github/windsekirun/yukarisynthesizer/dialog/PresetDialogFragment.kt
+++ app/src/main/java/com/github/windsekirun/yukarisynthesizer/dialog/PresetDialogFragment.kt
@@ -4,16 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.ObservableField
 import androidx.databinding.ObservableInt
 import com.github.windsekirun.bindadapters.observable.ObservableString
+import com.github.windsekirun.yukarisynthesizer.core.define.VoiceEngine
 import com.github.windsekirun.yukarisynthesizer.core.item.PresetItem
-import com.github.windsekirun.yukarisynthesizer.module.sheet.RoundedBottomSheetDialogFragment
 import com.github.windsekirun.yukarisynthesizer.databinding.PresetDialogFragmentBinding
+import com.github.windsekirun.yukarisynthesizer.module.sheet.RoundedBottomSheetDialogFragment
 
 /**
  * DialogFragment for adjust preset
  */
 class PresetDialogFragment : RoundedBottomSheetDialogFragment<PresetDialogFragmentBinding>() {
+    val selectedEngine = ObservableField<VoiceEngine>()
     val rangeProgress: ObservableInt = ObservableInt(10)
     val pitchProgress: ObservableInt = ObservableInt(5)
     val rateProgress: ObservableInt = ObservableInt(5)
@@ -31,29 +34,44 @@ class PresetDialogFragment : RoundedBottomSheetDialogFragment<PresetDialogFragme
         binding.fragment = this
 
         if (presetItem.id != 0L) {
+            selectedEngine.set(presetItem.engine)
+            title.set(presetItem.title)
             volumeProgress.set((presetItem.volume * 2).toInt())
             rateProgress.set(((presetItem.rate * 10) - 5).toInt())
-            pitchProgress.set(((presetItem.pitch* 10) - 5).toInt())
+            pitchProgress.set(((presetItem.pitch * 10) - 5).toInt())
             rangeProgress.set((presetItem.range * 10).toInt())
         } else {
+            selectedEngine.set(VoiceEngine.Yukari)
+            title.set("")
             volumeProgress.set(2)
             rateProgress.set(5)
             pitchProgress.set(5)
             rangeProgress.set(10)
-            title.set(presetItem.title)
+
         }
     }
 
     fun clickSave(view: View) {
         presetItem.apply {
+            this.engine = selectedEngine.get() ?: VoiceEngine.Yukari
             this.title = this@PresetDialogFragment.title.get()
             this.volume = volumeProgress.get() * 0.5
-            this.rate = ((rateProgress.get() + 1) * 0.1) + 0.5
+            this.rate = (rateProgress.get() * 0.1) + 0.5
             this.pitch = ((pitchProgress.get() + 1) * 0.1) + 0.5
             this.range = rateProgress.get() * 0.1
         }
 
+        callback.invoke(presetItem)
+
         dismiss()
+    }
+
+    fun clickYukari(view: View) {
+        selectedEngine.set(VoiceEngine.Yukari)
+    }
+
+    fun clickMaki(view: View) {
+        selectedEngine.set(VoiceEngine.Maki)
     }
 
     fun convertVolumeText(progress: Int): String = String.format("%.1f", progress * 0.5)
